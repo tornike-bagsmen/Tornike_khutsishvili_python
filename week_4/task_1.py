3a30e7e07eb3422a1fe1d603fdddecc710afedd2
tornike-bagsmen <tkhutsishvili7@gmail.com>
2024-09-23 22:54:27 +0400

    corection

--- week_4/task_1.py
+++ week_4/task_1.py
@@ -4,12 +4,11 @@ def main():
         
     quantity_of_player = int(input("please, write Quantity of players: "))
     
-    for i in range(quantity_of_player):
+    for _ in range(quantity_of_player):
         dice_0 = random.randint(1, 6)
         dice_1 = random.randint(1, 6)
         print(dice_0, dice_1)
-        quantity_of_player -= 1
-        
+                
 main()
 
 # import random
--- week_4/task_3.py
+++ week_4/task_3.py
@@ -2,12 +2,11 @@ import random
 
 def main():
     n_number = int(input("please, write number from 0 to 1000: "))
-    n = 1
+    
     if 0 < n_number < 1000:
-        for i in range(n_number) :
-            if n_number % n == 0:
-                print(n, end= " ")
-            n += 1
+        for i in range(1, n_number+1) :
+            if n_number % i == 0:
+                print(i, end= " ")
     else:
         print("wrong number!")
 
